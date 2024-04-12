4aeff11267ae76cf5d2e21cd0f7a77591949a12a
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-04-12 19:51:28 -0400

    Built api for creating issues

---
+++ prisma/client.ts
@@ -0,0 +1,17 @@
+//This code creates a single instance of prisma running and ensures we don't have multiple instances running.
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
